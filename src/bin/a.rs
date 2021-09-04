c5aba3da316d43a02f3d290d3a464213b582ef4e
mah-shee <masa@masatoshi.local>
2021-09-04 21:02:34 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,17 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        a: usize,
+        b: usize,
+        c: usize
+    }
+    if a == b {
+        println!("{}", c);
+    } else if b == c {
+        println!("{}", a);
+    } else if a == c {
+        print!("{}", b);
+    } else {
+        println!("0");
     }
 }
