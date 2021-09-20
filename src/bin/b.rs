5370d834e68525af4a3ae1926983bbb490e04aaa
mah-shee <masa@masatoshi.local>
2021-09-20 17:04:16 +0900

    Done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,10 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        k: usize
     }
+    // (1..k).sum() * n + (1..n).sum() * 100 * k
+    let ans: usize = (1..=k).sum::<usize>() * n + (1..=n).sum::<usize>() * 100 * k;
+    println!("{}", ans);
 }
