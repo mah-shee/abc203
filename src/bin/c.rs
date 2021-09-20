17d017969a5f62065e38875303020e175eed6ab0
mah-shee <masa@masatoshi.local>
2021-09-20 18:19:49 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,19 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        mut k: u128,
+        mut a_b: [(u128, u128); n]
     }
+    a_b.sort_by_key(|j| j.0);
+    let mut now = 0;
+    for i in 0..n {
+        if a_b[i].0 <= now + k {
+            k = k - (a_b[i].0 - now) + a_b[i].1;
+            now = a_b[i].0;
+        } else {
+            break;
+        }
+    }
+    println!("{}", now + k);
 }
